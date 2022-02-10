5eae6f6a2f06ec668299b4efa54a7415eaf69ee1
Nikita Korneich <80765148+moksyasha@users.noreply.github.com>
2022-02-10 14:09:52 +0700

    Add files via upload

---
+++ par1/core.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+#include <malloc.h>
+
+#define PI 3.14159265
+#define SIZE 10000080
+
+int main()
+{
+    double* array = NULL;
+    array = (double*)malloc((SIZE + 1) * sizeof(double));
+
+    double result = 0.0;
+
+    for (int i = 0; i < SIZE; i++) {
+        array[i] = sin(i * (2 * PI / SIZE));
+    }
+
+    for (int i = 0; i < SIZE; i++) {
+        result += array[i];
+    }
+
+    printf("%f\n", result);
+
+    free(array);
+    return 0;
+}
